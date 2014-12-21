4e8e5d0a07c2779b36e0f95e6cc643a84754d824
Philip Kahn <tigerhawkvok@gmail.com>
2014-12-21 12:35:31 -0800

    fix classic sanitize reference

--- classic-wysiwyg.php
+++ classic-wysiwyg.php
@@ -14,8 +14,8 @@ function parseBlock($block,$sanitize=true,$strip=true,$paragraph=true)
   if(!is_string($block)) return array(false,'error'=>'A string wasn\'t provided.');
   if($sanitize)
     {
-      require_once('/var/www/modular/db_hook.inc');
-      $parsed=sanitize($block);
+      # Requires DBHelper -- make sure it's included earlier!            
+      $parsed=DBHelper::statisSanitize($block);
     }
   else $parsed=$block;
   /***
